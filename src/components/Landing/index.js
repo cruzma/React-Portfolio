bb06063b7d06db7d598d6528660a62009a4013d5
Mateo <mtaycruz@gmail.com>
2021-06-11 14:24:47 -0400

    fixed nav bar and fixed hero background

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Landing() {
   return (
     <section className="hero">
-      <div>
+      <div className="hero-logo">
         <h3>Hi, I'm</h3>
         <h1>Mateo Cruz</h1>
       </div>
