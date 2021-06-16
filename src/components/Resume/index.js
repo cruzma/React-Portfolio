42342c4adeb08fc0edccfb6c424dea8d66e2c604
Mateo <mtaycruz@gmail.com>
2021-06-16 15:49:33 -0400

    resume link work now

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -7,21 +7,21 @@ function Resume(){
         
         <section className="hero">
             <div className="proficiencies">
-                <p>Download my resume here: </p><a href="../../assets/MateoCruz3resume.docx" download>Resume</a>
+                <p>Download my resume here: </p><a href={require("../../assets/Mateo Cruz 3 resume.pdf").default} target="_blank" id="resume">Resume</a>
                 <br/>      
                 <h3>My Proficiencies</h3>
                 <ul>
-                    <li><img src={require("../../assets/images/html-5.svg").default}></img></li>
-                    <li><img src={require("../../assets/images/css.svg").default}></img></li>
-                    <li><img src={require("../../assets/images/javascript.svg").default}></img></li>
-                    <li><img src={require("../../assets/images/react.svg").default}></img></li>
-                    <li><img src={require("../../assets/images/bootstrap.png").default}></img></li>
-                    <li><img src={require("../../assets/images/api.svg").default}></img></li>
-                    <li><img src={require("../../assets/images/Node.js_logo.svg").default}></img></li>
-                    <li><img src={require("../../assets/images/Npm-logo.svg").default}></img></li>
-                    <li><img src={require("../../assets/images/github.svg").default}></img></li>
-                    <li><img src={require("../../assets/images/heroku.svg").default}></img></li>
-                    <li><img src={require("../../assets/images/mongodb.svg").default}></img></li>
+                    <li><img src={require("../../assets/images/html-5.svg").default} alt=""></img></li>
+                    <li><img src={require("../../assets/images/css.svg").default} alt=""></img></li>
+                    <li><img src={require("../../assets/images/javascript.svg").default} alt=""></img></li>
+                    <li><img src={require("../../assets/images/react.svg").default} alt=""></img></li>
+                    <li><img src={require("../../assets/images/bootstrap.png").default} alt=""></img></li>
+                    <li><img src={require("../../assets/images/api.svg").default} alt=""></img></li>
+                    <li><img src={require("../../assets/images/Node.js_logo.svg").default} alt=""></img></li>
+                    <li><img src={require("../../assets/images/Npm-logo.svg").default} alt=""></img></li>
+                    <li><img src={require("../../assets/images/github.svg").default} alt=""></img></li>
+                    <li><img src={require("../../assets/images/heroku.svg").default} alt=""></img></li>
+                    <li><img src={require("../../assets/images/mongodb.svg").default} alt=""></img></li>
                 </ul>
             </div>
         </section>
